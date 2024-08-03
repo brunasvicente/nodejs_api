838665c2fbbbd14c923e62149a49857f19b6adf4
Bruna Vicente <brutronica931@gmail.com>
2024-08-03 00:49:14 -0300

    Estudando API's

--- api.js
+++ api.js
@@ -4,19 +4,19 @@ servidor.use( express.json() );
 
 servidor.get('/helloworld', (req, resp) => {
     resp.send('Hello World')
-})
+});
 
 servidor.get('/mensagem/boasvindas/:nome', (req, resp) => {
     let nome = req.params.nome
     resp.send(`Olá, seja bem-vindo(a) ${nome}!!!`)
-})
+});
 
 servidor.get('/calculadora/somar', (req, resp) => {
     let n1 = Number(req.query.n1)
     let n2 = Number(req.query.n2)
     let soma = n1 + n2
     resp.send(`${n1} + ${n2} = ${soma}`)
-})
+});
 
 servidor.post('/media', (req, resp) => {
     let n1 = req.body.nota1
@@ -24,7 +24,7 @@ servidor.post('/media', (req, resp) => {
     let n3 = req.body.nota3
     let media = (n1 + n2 + n3) /3
     resp.send(`A média é ${media}`)
-})
+});
 
 servidor.post('/dobro', (req, resp) => {
     let nums = req.body.numeros
@@ -35,7 +35,7 @@ servidor.post('/dobro', (req, resp) => {
     }
 
     resp.send('O dobro dos numeros é ' + nums2)
-})
+});
 
 servidor.post('/loja/pedido', (req, resp) => {
     let total = req.body.total
@@ -50,7 +50,7 @@ servidor.post('/loja/pedido', (req, resp) => {
     }
 
     resp.send(`Total: R$ ${total.toFixed(2)}`)
-})
+});
 
 servidor.post('/loja/pedido/completo', (req, resp) => {
     let parcelas = req.body.parcelas
@@ -58,8 +58,8 @@ servidor.post('/loja/pedido/completo', (req, resp) => {
     let cupom = req.query.cupom
 
     let total = 0
-    for (let product of itens) {
-        total += product.preco
+    for (let produto of itens) {
+        total += produto.preco
     }
 
     if (parcelas > 1) {
@@ -70,7 +70,7 @@ servidor.post('/loja/pedido/completo', (req, resp) => {
     }
     
     resp.send(`O total é R$ ${total.toFixed(2)}`)
-})
+});
 
 servidor.listen(5001,
     () => console.log('---> API subiu com sucesso na porta 5001!'));
